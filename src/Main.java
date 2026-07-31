80b24ae0d469b4fde8804a903dbcf40acb6ecf02
Wesley Neves <wesleyneves675@gmail.com>
2026-07-31 14:36:41 -0400

    commit com a classe main com os dados de operacoes matematica

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	int valor1, valor2;
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.print("Informe o 1o valor: ");
+	valor1 = scanner.nextInt();
+	
+	System.out.print("\nInforme o 2o valor: ");
+	valor2 = scanner.nextInt();
+	
+	System.out.println("Soma: "+(valor1 + valor2));
+	System.out.println("Dif.: "+(valor1 - valor2));
+	System.out.println("Multiplicação: "+(valor1 * valor2));
+	System.out.println("Divisão: "+(valor1 / valor2));
+	}
+}
